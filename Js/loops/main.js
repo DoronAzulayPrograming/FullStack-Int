d9058875672ebc7971a0783b5c8d590d05d999de
doron azulay <doronazulaycodeing@gmail.com>
2022-09-15 16:21:53 +0300

    add lesson 12 13
    vars if loops

---
+++ Js/loops/main.js
@@ -0,0 +1,40 @@
+// ---------------------1----------------------
+// console.log("hello")
+// let a:number , b:number , c:number
+// a = 14
+// b = 2
+// c = a + b
+// console.log(c)
+// b *= 3
+// console.log(b)
+// let res = a + b
+// console.log(res / a)
+// //////////////////////////
+// //console.log((a + b) / a)
+// if(b > 10){
+//     b /= 2
+//     console.log(b)
+// }else{
+//     console.log(b)
+// }
+// ////////////////////
+// // if(b > 10){
+// //     b/=2
+// // }
+// //console.log(b)
+// if(c > 1 && c < 10){
+//     console.log("in range")
+// }
+// ---------------------2----------------------
+// let days = 1
+// let weeks = days / 7  //  2 * 7 = 14
+// weeks = parseInt(weeks + "")
+// let newDays = days - (weeks * 7)
+// console.log("you save "+ weeks +" weeks and "+ newDays +" days in omer")
+// ---------------------3----------------------
+// let num = 321 
+// console.log(num % 10)
+// num = parseInt((num / 10)+ "")
+// console.log(num % 10)
+// num = parseInt((num / 10)+ "")
+// console.log(num % 10)
---
+++ Js/varsAndIf/main.ts
@@ -0,0 +1,69 @@
+//--------------------------1--------------------------------------
+// let a:number = 0, b:number = 0, c:number = 0;
+
+// console.log("hello")
+
+// a = 4;
+// b = 2;
+// c = a + b;
+// console.log(c);
+// b *= 3;
+// console.log(b);
+// console.log((a+b)/ a);
+
+// if(b > 10){
+//     b /= 2
+// }
+
+// console.log(b);
+
+
+// if(c > 1 && c < 10){
+//     console.log("in range");
+// }
+
+
+//-----------------------------2------------------------------------
+
+// let days = 16
+// let weeks = parseInt((days / 7).toString())
+// let newDays = days - (weeks * 7)
+// console.log("you save " + weeks + " weeks and " + newDays + " days in omer")
+
+//-----------------------------3------------------------------------
+
+// let num = 321
+// console.log( parseInt( (num % 10).toString() ) )
+// num/=10
+// console.log( parseInt( (num % 10).toString() ) )
+// num/=10
+// console.log( parseInt( (num % 10).toString() ) )
+
+
+
+//-----------------------------6------------------------------------
+
+// for(let i = 2; i < 50; i++){
+//     if(i % 3 == 0){
+//         console.log(i);
+//     }
+// }
+
+
+//-----------------------------7------------------------------------
+
+// let odd = 0
+// let even = 0
+// for(let i = 2; i < 50; i++){
+//     if(i % 3 == 0){
+//         console.log(i);
+//         if(i % 2 == 0){
+//             even++
+//         }else{
+//             odd++
+//         }
+//     }
+// }
+
+// console.log("odd numbers: " + odd);
+// console.log("even numbers: " + even);
