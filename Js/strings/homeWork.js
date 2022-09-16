3006aaf06dc0468a84832abe654a87edce2377e8
doron azulay <doronazulaycodeing@gmail.com>
2022-09-16 17:16:10 +0300

    add strings
    strings and base array
    and last home work

---
+++ Js/strings/homeWork.js
@@ -0,0 +1,28 @@
+// ----------------------1---------------------
+// let odd = 0
+// let even = 0
+// for(let i = 2; i < 50; i++){
+//     if(i % 3 == 0){
+//         if(i % 2 == 0){
+//             even++
+//         }else{
+//             odd++
+//         }
+//     }
+// }
+// console.log("odd numbers: " + odd)
+// console.log("even numbers: " + even)
+// ----------------------2---------------------
+// let a = 4 , b = 2 // 4+4 = 8
+// let res = 0
+// for(let i = 0; i < b; i++){
+//     res += a
+// }
+// console.log(res)
+// ----------------------3---------------------
+// let a = 5 , b = 5 // 5*5 = 25
+// let res = 1
+// for(let i = 0; i < b; i++){
+//     res *= a
+// }
+// console.log(res)
---
+++ Js/strings/main.js
@@ -0,0 +1,39 @@
+// let str = "hello welcome"
+// for(let i = 0; i < str.length; i++){
+//     str[i]
+//     console.log()
+// }
+// let A:number
+// let Z:number
+// A = "A".charCodeAt(0)
+// Z = "Z".charCodeAt(0)
+// let str = ""
+// for(let char = A; char <= Z; char++){
+//     str += String.fromCharCode(char)
+// }
+// console.log(str)
+// let str = "hello and welcome"
+// let count = 0
+// for(let i = 0; i < str.length; i++)
+//     if(str[i] == " ")
+//         count++
+// if(count > 0)
+//     count++
+// console.log("words in text: " + count)
+// let str = "!)hE&l )l!!o"
+// let text = ""
+// for(let i = 0; i < str.length; i++ ){
+//     if(
+//      (str[i] >= "a" && str[i] <= "z") ||
+//      (str[i] >= "A" && str[i] <= "Z")
+//      ){
+//         text += str[i]
+//     }
+// }
+// console.log(text)
+var list = [0, 0, 0, 0];
+console.log(list);
+for (var i = 0; i < list.length; i++) {
+    list[i] = i + 1;
+}
+console.log(list);
