f174ad4a2f21004e132ec0f2349d54b580b65655
doron azulay <doronazulaycodeing@gmail.com>
2022-10-04 13:25:18 +0300

    add last two lesson
    Functions
    Recursion

---
+++ Js/Functions/main.ts
@@ -0,0 +1,71 @@
+//----------3-----------
+
+
+// // print min value from the 2 parameters
+// function printMin(a:number, b:number){
+//     if(a <= b)
+//         console.log(a)
+//     else console.log(b)
+// }
+
+// // print from 9 to 1 in one line
+// function printFromNineToOne(){
+//     let str = ""
+//     for(let i = 9; i > 0; i--){
+//         str += i
+//     }
+//     console.log(str)
+// }
+
+// function printOddNumbers(){
+//     let str = ""
+//     for(let i = 8; i > 1; i-=2){
+//         str+=i
+//     }
+//     console.log(str)
+// }
+
+// function kefel(a:number,b:number){
+//     let res = 0
+//     for(let i = 0; i < b; i++){
+//         res += a
+//     }
+    
+//     console.log(res)
+// }
+
+// function hezka(a:number,b:number){
+//     let res = 1
+//     for(let i = 0; i < b; i++){
+//         res *= a
+//     }
+    
+//     console.log(res)
+// }
+
+// hezka(5,3)
+
+// let person = {
+//     name:"",
+//     age:0,
+//     func:function printhi(){
+//         console.log("hi")
+//     }
+// }
+
+// console.error()
+// console.error = hezka
+// console.error(4,2)
+
+// let a = 4
+// console.log(a)
+// a = 5
+// console.log(a)
+
+
+
+
+// console.log(10)
+
+
+console.log(document.getElementsByTagName("h1"))
---
+++ Js/Recursion/main.js
@@ -0,0 +1,54 @@
+// for(let i = 1; i < 10; i++){
+//     console.log(i)
+// }
+// function print(i){
+//     if(i == 0)
+//         return
+//     console.log(i)
+//     print(i-1)
+// }
+// print(3)
+// function printHello(index){
+//     if(index == 10)
+//         return
+//     printHello(index + 1)
+//     console.log("hello")
+// }
+// printHello(1)
+//----------1-------------
+// let arr = [1,2,1,1]
+// function sumArr(arr:number[], i:number){
+//     if(i == 0)
+//         return arr[i]
+//         let prev = sumArr(arr, i - 1)
+//         let current = arr[i]
+//     return prev + current
+// }
+// let res = sumArr(arr, arr.length - 1)
+// console.log(res)
+//----------2-------------
+// let arr = [1,1,1,1]
+// function addArrys(arr1, arr2, i){
+//     if(i == 0){
+//         arr1[i] += arr2[i]
+//         return arr1
+//     }
+//     arr1[i] += arr2[i]
+//     return addArrys(arr1,arr2, i -1)
+// }
+// let res = addArrys(arr,arr,arr.length - 1)
+// console.log(res)
+//----------3-------------  4*2 = 4+4 = 8
+// function kefel(a,b){
+//     if(b == 1)
+//         return a
+//     return kefel(a,b-1) + a
+// }
+// console.log(kefel(4,2))
+//----------4-------------
+// function hezka(a,b){
+//     if(b == 1)
+//         return a
+//     return hezka(a,b-1) * a
+// }
+// console.log(hezka(5,3))
